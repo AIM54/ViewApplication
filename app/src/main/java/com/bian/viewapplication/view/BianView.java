18640692b294797c3b6704019f295513b4cb18a1
bian <15021741859@163.com>
2018-06-18 18:16:29 +0800

    实验下android canvas 图层的api

--- app/src/main/java/com/bian/viewapplication/view/BianView.java
+++ app/src/main/java/com/bian/viewapplication/view/BianView.java
@@ -11,6 +11,8 @@ import android.view.View;
 
 import com.bian.viewapplication.R;
 
+import java.text.NumberFormat;
+
 /**
  * Created by bian on 18-6-16.
  */
@@ -19,7 +21,8 @@ public class BianView extends View {
     private Paint mPaint;
     private int mHeight,mWidth;
     private int radius;
-    private RectF arcRectF;
+    private RectF arcRectF,tempRectF;
+    private Paint linePaint;
     public BianView(Context context) {
         super(context);
         initPaint();
@@ -33,8 +36,11 @@ public class BianView extends View {
     private void initPaint() {
         mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setStrokeWidth(getContext().getResources().getDimension(R.dimen.dp3));
         mPaint.setColor(Color.GREEN);
+        linePaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaint.setStrokeWidth(getContext().getResources().getDimension(R.dimen.dp3));
+        linePaint.setColor(Color.YELLOW);
+        linePaint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
@@ -43,10 +49,16 @@ public class BianView extends View {
         mWidth=getMeasuredWidth();
         radius=Math.min(mWidth,mHeight)/2;
         arcRectF=new RectF(0,0,radius,radius);
+        tempRectF=new RectF(0,0,mWidth,mHeight);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-      canvas.drawArc(arcRectF,0,120,true,mPaint);
+     canvas.drawCircle(100,100,100,mPaint);
+     canvas.saveLayerAlpha(tempRectF,0x34,Canvas.ALL_SAVE_FLAG);
+     mPaint.setColor(Color.MAGENTA);
+     canvas.drawCircle(100,200,100,mPaint);
+     canvas.saveLayerAlpha(tempRectF,0x66,Canvas.ALL_SAVE_FLAG);
+     canvas.drawCircle(300,100,100,mPaint);
     }
 }
