f2ecb03d136ddf3f4979099c689ce32d97a8a304
1657572284@qq.com <1657572284@qq.com>
2018-06-19 16:06:29 +0800

    升级到java8

--- app/src/main/java/com/bian/viewapplication/MainActivity.java
+++ app/src/main/java/com/bian/viewapplication/MainActivity.java
@@ -1,15 +1,24 @@
 package com.bian.viewapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
+import android.widget.Button;
+
+import com.bian.viewapplication.activity.ListViewActivity;
 
 public class MainActivity extends AppCompatActivity {
     private SurfaceView mSurfaceView;
-
+    private Button listButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        listButton.setOnClickListener(v -> new Intent(this, ListViewActivity.class));
     }
 }
